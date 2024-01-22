146db5e7cbedd5697eb9481c6bd68a64df64f852
Nguyễn Văn Định <43395337+nvdinh185@users.noreply.github.com>
2024-01-22 19:46:34 +0700

    Add files via upload

---
+++ baitap1.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+const util = require('util');
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "tintuc"
+});
+
+
+// node native promisify
+const query = util.promisify(conn.query).bind(conn);
+
+const createDB = 'CREATE DATABASE tintuc COLLATE utf8_unicode_ci';
+
+const sqlCreate = `CREATE TABLE danhmuctin(
+                        id_danhmuctin int NOT NULL AUTO_INCREMENT,
+                        tendanhmuctin varchar(255),
+                        PRIMARY KEY (id_danhmuctin)
+                    )`;
+
+const sqlInsert = `INSERT INTO danhmuctin(id_danhmuctin, tendanhmuctin) VALUES
+                    (1, "Toàn cảnh nhật Bản"), (2, "Tin tức Việt Nhật"), 
+                    (3, "Tin tức giải trí"), (4, "Du học Nhật Bản"), 
+                    (5, "Tin thể thao"), (6, "Tin xã hội")`;
+
+async function createAndInsertData() {
+    try {
+        // Tạo db
+        // const dataCrDB = await query(createDB);
+        // console.log(dataCrDB);
+
+        // Tạo table
+        const dataCrTB = await query(sqlCreate);
+        console.log(dataCrTB);
+
+        // Insert data
+        const dataIs = await query(sqlInsert);
+        console.log(dataIs);
+    } catch (err) {
+        console.log('Lỗi: ' + err);
+    } finally {
+        conn.end();
+    }
+}
+createAndInsertData();
---
+++ baitap2.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+const util = require('util');
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "shophoa2"
+});
+
+
+// node native promisify
+const query = util.promisify(conn.query).bind(conn);
+
+const createDB = 'CREATE DATABASE shophoa2 COLLATE utf8_unicode_ci';
+
+const sqlCreate = `CREATE TABLE loai_hoa(
+                    id_cat INT NOT NULL AUTO_INCREMENT, 
+                    ten_cat VARCHAR(50) NOT NULL,
+                    PRIMARY KEY (id_cat)
+                    )`;
+
+const sqlInsert = `INSERT INTO loai_hoa (ten_cat) VALUES ("Khai trương"), ("Sin nhật"), ("Ngày cưới"),
+                    ("Kỹ niệm"), ("Tình yêu"), ("Hoa bán"),
+                    ("Hoa chia buồn"), ("Hoa hạnh phốc"),("Hoa bí"), ("Hoa bầu")`;
+
+async function createAndInsertData() {
+    try {
+        // Tạo db
+        // const dataCrDB = await query(createDB);
+        // console.log(dataCrDB);
+
+        // Tạo table
+        const dataCrTB = await query(sqlCreate);
+        console.log(dataCrTB);
+
+        // Insert data
+        const dataIs = await query(sqlInsert);
+        console.log(dataIs);
+    } catch (err) {
+        console.log('Lỗi: ' + err);
+    } finally {
+        conn.end();
+    }
+}
+createAndInsertData();
